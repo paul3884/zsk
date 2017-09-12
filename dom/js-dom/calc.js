930ebf8c24a2910b2b10fe85333ffda6db06b8b2
paul3884 <pawel1mendel@gmail.com>
2017-09-12 17:03:32 +0200

    3

--- dom/js-dom/calc.js
+++ dom/js-dom/calc.js
@@ -19,6 +19,10 @@ function liczPoleStozka(r, l){
 
     return polePodstawy + polePowBocznej;
 }
+//Parametrami funkcji jest tabela przechowująca
+//obliczenia na danych obiekcie,
+//wybrany przez użytkownika obiekt do wypisania na stronie,
+//oraz łańcuch mówiący czym jest wynik wyświetlany na stronie
 function wypiszWynik(wynik, obiekt, informacja){
     var s = "";
 
@@ -27,6 +31,11 @@ function wypiszWynik(wynik, obiekt, informacja){
 
     for(var i = 0; i < wynik.length; i++){
 
+        //Pętla wykona sie tyle razy ile jest elementów w tablicy [2]
+        //do zmiennej "s" dopisywana jest tekst z wynikiem liczbowym zaokrąglonym
+        //do 4 miejsc po przecinku
+        //rozbijany jest również pobrany łańchuch na tabele łańcuchów
+        //informacja.split(" ")[i] - wybrany zostanie element i z powstałej tabeli
         s += "Wynik " + i + ": " + wynik[i].toPrecision(4) + " | "+ informacja.split(" ")[i] + "<br>";
 
     }
@@ -36,20 +45,30 @@ function wypiszWynik(wynik, obiekt, informacja){
 
 
 function calc(){
+    //Wartość wybrana z formularza z elementów o wspólnej nazwie [figure]
     var figura = form.elements["figure"].value;
 
-    promien = parseInt(document.getElementById("r").value,10);
-    wStozka = parseInt(document.getElementById("h").value,10);
-    twStozka = parseInt(document.getElementById("l").value,10);
+    //Pobranie wpisanych przez użytkownika elementów
+    //Konwersja na postać liczbową o podstawie dziesiętnej
+    var promien = parseInt(document.getElementById("r").value,10);
+    var wStozka = parseInt(document.getElementById("h").value,10);
+    var twStozka = parseInt(document.getElementById("l").value,10);
 
+    //Zależnie od wybranego przez użytkownika elementu Koło lub Stożek
+    //Wykona się odpowiednia część kodu
     switch(figura){
         case "stozek":{
             console.log("Liczymy stozek");
+
+            //Zmienna pomocnicza do wyświetlenia końcowego wyniku
             var tekst = "pole objętość";
 
+            //Stworzenie nowej tabeli
             result = new Array();
-
+            //Wprowadzenie 2 nowych elementów tablicy za pomocą funkcji zwracających
+            //liczby
             result.push(liczPoleStozka(promien, twStozka), liczObjetoscStozka(promien, wStozka));
+
 
             wypiszWynik(result, figura, tekst);
 
@@ -58,10 +77,15 @@ function calc(){
         }
         case "kolo":{
             console.log("Liczymy kolo");
+            //Zmienna pomocnicza do wyświetlenia końcowego wyniku
             var tekst = "pole obwód";
-            result = new Array();
 
+            //Stworzenie nowej tabeli
+            result = new Array();
+            //Wprowadzenie 2 nowych elementów tablicy za pomocą funkcji zwracających
+            //liczby
             result.push(liczPoleKola(promien), liczObwodKola(promien));
+
 
             wypiszWynik(result, figura, tekst);
 
