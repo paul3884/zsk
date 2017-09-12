6c99033912b1a93fd53183a8ab9da99fa7ed67ff
paul3884 <pawel1mendel@gmail.com>
2017-09-12 12:13:30 +0200

    11

--- git/js/funkcje.js
+++ git/js/funkcje.js
@@ -9,11 +9,29 @@ function PoleProstokata(a,b){
     return a*b;
 }
 
-var _a = prompt("A: ");
-var _b = prompt("B: ");
+/*var _a = prompt("A: ");
+var _b = prompt("B: ");*/
 
 
-console.log(PoleProstokata(_a, _b));
+/*console.log(PoleProstokata(_a, _b));
 
 var name = "daniel";
-witaj(name);
+witaj(name);*/
+
+
+function poleObjetosc(szerokosc, dlugosc, wysokosc){
+    var pole = szerokosc * dlugosc;
+    var objetosc = pole * wysokosc;
+
+    var wynik = [pole,objetosc];
+    return wynik;
+
+}
+var x = prompt("Podaj wartosc, 0 - pole, 1 - objetosc");
+
+var a = prompt("Podaj a: ");
+var b = prompt("Podaj b: ");
+var c = prompt("Podaj c: ");
+
+var poleProstokata = poleObjetosc(a,b,c)[x];
+console.log(poleProstokata);
---
+++ git/js/zadanie.js
@@ -0,0 +1,5 @@
+var pi = 3.14;
+
+function poleKola(r){
+    return Math.PI * Math.pow(r);
+}
