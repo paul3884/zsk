b1f39d0f98fc0d9b11998cee1b8f5002df01f8fa
paul3884 <pawel1mendel@gmail.com>
2017-10-20 13:15:05 +0200

    asa

---
+++ git/PHP/operacjeZmiennych.php
@@ -0,0 +1,65 @@
+
+<?php
+
+echo  "ORKI, ORKI Z POZNANIA" . '<br>';
+
+//operatory
+
+//operator potęgowanie [**]
+$potegowanie = 2**3;
+
+//echo "$potegowanie";
+
+//modulo
+
+$modulo = 13%3;
+
+//echo "$modulo";
+
+// + , - , * , / ,
+
+//operatory bitowe: w PHP: and &, or |, not -, xor *, przesunięcie bitowe w lewo <<, przesunięcie bitowe >>
+
+
+
+
+
+/*$x = 2;
+
+echo $x++;  //2
+echo ++$x; // 4
+echo $x; //4
+
+$y = $x++; //4
+
+echo $y; //4
+
+$y = ++$x;
+
+echo $y; //6
+echo ++$y; //7*/
+
+
+
+//operator łączenia łańcuchów
+
+$t1 = "Pierwszy tekst";
+$t2 = "Drugi tekst";
+$t3 ="6";
+
+$liczba = 14;
+
+//echo $t1.$t2.$liczba;
+
+//operatory rzutowania
+
+//bool,int,float,string,array,object,unset
+
+$dwa = (int)$t1;
+$trzy = (int)$t3;
+
+
+//echo $dwa;
+echo $trzy;
+
+?>
--- git/PHP/zmienne.php
+++ git/PHP/zmienne.php
@@ -1,22 +1,92 @@
-<!doctype html>
-<html>
-    <head>
 
-        <title>Lekcja 3</title>
-        <meta charset="utf-8">
-
-    </head>
-    <body>
-
-        <?
+<?php
 
 
-        ?>
+$a = 16;
+$_liczba =5;
+$liczba2 = 5;
 
-    </body>
+$imie = "Janusz";
+$Imie = "Adam";
+
+$imię_ = "Adam"; //można dać polskie znaki do zmiennych;
+$wartość = "Adam"; //można dać polskie znaki do zmiennych;
+
+echo "$liczba2"; //10
+echo $liczba2; //10
+echo '$liczba2' . "<br>"; //liczba2
 
 
+//typy zmiennych
+//typ skalarny [prosty]
 
-</html>
+//typ boolean
+
+$prawda = true;
+$prawda = false;
+
+//typ integer
+
+$calkowita = 200;
+
+$szesnastkowa = 0xA;
+
+//echo "$szesnastkowa" . "<br>";
+
+$osemkowa= 010;
+
+//echo "$osemkowa" . "<br>";
+
+$binarna= 0b111;
+
+//echo "$binarna";
+
+//typ float
+
+$zmiennoprzecinkowa = 5.2;
+
+//echo "$zmiennoprzecinkowa";
+
+//typ string
+
+$a = 5.2;
+
+echo "$a";
+
+//składnia heredoc
+
+$im = "Krystyne";
+
+//Przypisanie etykiety
+$heredoc = <<<TEKST
+Mam horom curke $im
+TEKST;
+
+//echo "$heredoc";
+
+//składnia nowdoc
+
+$tekst = "Mowak";
+$nowdoc = <<<'ETYKIETA'
+Moje nazwisko: $tekst
+
+ETYKIETA;
+
+//echo "$nowdoc";
+
+//typy zlozone
+    //typ array
+    //typ object
+
+//typ specjalny
+    //typ resource - odwołanie do zewnętrznego zasobu
+    //typ null
+
+$inna = null;
+$inna = NULL;
+
+
+echo "Zmienna o nazwie \$inna ma wartość: ", gettype($inna);
+?>
 
 
